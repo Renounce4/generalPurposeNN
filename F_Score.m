5645ad24bc131dd976453ca49f71c5b1c7b74528
William Estey <westey@INSIDE.ATC-NYCORP.COM>
2017-10-16 14:35:16 -0400

    Added more doc to F_Score

--- F_Score.m
+++ F_Score.m
@@ -1,6 +1,7 @@
 function F = F_Score(y, p, b)
 % USAGE: function F = F_Score(y, p, b=1)
 % Calculates the F score of your predictions (p) given ground truth labels (y), and using bias (b).
+% F = the effectiveness of retrieval with b times as much importance given to recall as precision.
 % Assumes y and p are binary vectors of the same length.
 
 if ~exist('b', 'var') || isempty(b)
